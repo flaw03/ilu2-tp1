716c827a7579472ebe8e13f082f1f5569d23158c
flaw03 <100543813+flaw03@users.noreply.github.com>
2023-02-15 11:57:11 +0100

    classe interne marche

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -8,10 +8,75 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
+	private Marche[] marche;
+	
+	
+	private static class Marche{
+		
+		private static  Etal[] etals;
+		private static int nbrEtal = 0;
+		
+		private Marche(int nbrEtal) {
+			etals = new Etal[nbrEtal];
+			this.nbrEtal = nbrEtal;
+			for(int i = 0 ; i<nbrEtal ;i++) {
+				etals[i].occuperEtal(null, null, 0);
+				etals[i].libererEtal();
+			}
+		}
+		
+		
+		private static void utiliserEtal(int indiceEtal, Gaulois vendeur,String produit,int nbProduit) {
+			Etal etal = etals[nbrEtal];
+			etal.occuperEtal(vendeur, produit, nbProduit);
+		}
+	
+		private static int trouverEtalLibre() {
+			int i = 0;
+			while (i<nbrEtal) {
+				i++;
+				if (etals[i].isEtalOccupe()) {
+					return i;
+				}
+			}
+			return -1;
+		}
+		
+		private static Etal[] touverEtals(String produit) {
+			Etal[] etalProduit = new Etal[nbrEtal];
+			int i1=0;
+			for(int i = 0 ; i<nbrEtal ; i++) {
+				if (etals[i].contientProduit(produit)) {
+					etalProduit[i1] = etals[i];
+					i1++;
+				}
+			}
+			return etalProduit;
+		}	
+		
+		private static Etal touverVendeur(Gaulois gaulois) {
+			Etal etalTrouver = null;
+			int i = 0;
+			while(etals[i].getVendeur() == gaulois) {
+				i++;
+			}
+				return  etalTrouver;
+		}
+		
+		private static String afficherMarche() {
+			String texte = "Les etal du marcher :";
+			for (int i = 0 ; i<nbrEtal;i++) {
+				texte = texte +"/n" +etals[i].afficherEtal();
+			}
+			return texte;
+		}
+		
+	}
 
-	public Village(String nom, int nbVillageoisMaximum) {
+	public Village(String nom, int nbVillageoisMaximum, int nbEtalsMaximum ) {
 		this.nom = nom;
 		villageois = new Gaulois[nbVillageoisMaximum];
+		marche = new Marche[nbEtalsMaximum];
 	}
 
 	public String getNom() {
@@ -56,4 +121,25 @@ public class Village {
 		}
 		return chaine.toString();
 	}
+	
+	public String installerVendeur(Gaulois vendeur,String produit, int nbProduit) {
+		marche.t
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
